f5d4fb4507f9c95961503e8a9d0d84b34ab6780b
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-12 16:18:11 +0200

    remove pom.xml from .gitignore

--- src/main/java/InventoryDelegate.java
+++ src/main/java/InventoryDelegate.java
@@ -1,7 +1,7 @@
-public interface InventoryDelegate<T extends Item>  {
+public interface InventoryDelegate<T extends Item >  {
     void addItem(T item);
     void removeItem(int itemID) throws ItemNotFoundException, InsufficientStockException;
     void viewItems();
     public void editItem(Item taskYouWantToEdit, String editedName);
     public void reduceItemQuantity(int itemID) throws ItemNotFoundException, InsufficientStockException;
-    }
+}
