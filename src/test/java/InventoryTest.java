12eada3ea31df8669723b5444abcba26b43f3d2c
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-13 16:53:47 +0200

    add display names for test methods

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,12 +14,14 @@ class InventoryTest {
     }
 
     @Test
+    @DisplayName("item is added to the inventory")
     void addItem() {
         inventory.addItem(new FoodItem(1, "Hamburger", 2));
         assertTrue(inventory.viewItems().contains("Hamburger"));
     }
 
     @Test
+    @DisplayName("item is removed from the inventory")
     void removeItem() {
         inventory.addItem(new FoodItem(2, "Potatoes", 4));
         assertDoesNotThrow(() -> inventory.removeItem(2));
@@ -26,17 +29,20 @@ class InventoryTest {
     }
 
     @Test
+    @DisplayName("ItemNotFoundException thrown when removing item")
     void RemoveItemNotFoundException() {
         assertThrows(ItemNotFoundException.class, () -> inventory.removeItem(1));
     }
 
     @Test
+    @DisplayName("InsufficientStockException thrown when removing item")
     void RemoveItemInsufficientStockException() {
         inventory.addItem( new FoodItem(1, "Tomato", 0));
         assertThrows(InsufficientStockException.class, () -> inventory.removeItem(1));
     }
 
     @Test
+    @DisplayName("reduce item quantity by one using item id")
     void reduceItemQuantity() {
         FoodItem cheese = new FoodItem(2, "Cheese", 5);
         inventory.addItem(cheese);
@@ -45,17 +51,20 @@ class InventoryTest {
     }
 
     @Test
+    @DisplayName("ItemNotFoundException thrown when reducing item quantity")
     void ReduceItemQuantityItemNotFoundException() {
         assertThrows(ItemNotFoundException.class, () -> inventory.reduceItemQuantity(23));
     }
 
     @Test
+    @DisplayName("InsufficientStockException thrown when reducing item quantity")
     void ReduceItemQuantityInsufficientStockException() {
         inventory.addItem(new FoodItem(1, "Crackers", 0));
         assertThrows(InsufficientStockException.class, () -> inventory.reduceItemQuantity(1));
     }
 
     @Test
+    @DisplayName("view items in inventory")
     void viewItems() {
         inventory.addItem(new FoodItem(5, "Chocolate", 10));
         inventory.addItem(new FoodItem(6, "Eggs", 6));
@@ -65,6 +74,7 @@ class InventoryTest {
     }
 
     @Test
+    @DisplayName("edit an item in the inventory")
     void editItem() {
         FoodItem chips = new FoodItem(8, "Chips", 3);
         inventory.addItem(chips);
